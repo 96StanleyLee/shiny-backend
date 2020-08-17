98c6f36be1ef9a098c15e4231955d346739d4904
96StanleyLee <96stanleylee@gmail.com>
2020-08-17 13:54:18 -0400

    user controller fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+    
+    def index
+        users = User.all 
+        render json: users
+    end 
+
+    def show
+        user = User.find(params[:id])
+        if user
+            render json: user
+        else
+            render json: {error: 'User not found'}
+        end 
+
+    end 
+
 end
