3216cf61cc019d116b3d85d9c139b8c04770f76d
96StanleyLee <96stanleylee@gmail.com>
2020-08-17 13:59:18 -0400

    extra seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 puts 'Deleting all current users'
+User.destroy_all
 
 User.create(first_name: 'Rock Vincent', last_name: 'Guitard', password_digest: 'test', email:'test@gmail.com', profile_picture:'https://miro.medium.com/max/3150/1*uWef_ndWcLUrdxS9v0J5xg.jpeg')
+User.create(first_name: 'Stanley', last_name: 'Lee', password_digest: 'test', email:'stanley@gmail.com', profile_picture:'https://cottagelife.com/wp-content/uploads/2020/08/Cottage-Life-Justin-Trudeau.jpg')
